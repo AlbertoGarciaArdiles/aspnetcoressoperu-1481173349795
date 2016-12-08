aa9d3fe614a35c39b681731ccee50590e56efa94
ALBERTO FLABIO GARCIA ARDILES <afgarcia@pe.ibm.com>
2016-12-08 08:03:44 -0500

    c
    c

--- src/smartadmin/Program.cs
+++ src/smartadmin/Program.cs
@@ -18,13 +18,12 @@ namespace SmartAdmin
           //      .AddJsonFile("hosting.json", optional: true)
           //      .Build();
 
-          var config = new ConfigurationBuilder()
-              .AddCommandLine(args)
-              .Build();
+          //var config = new ConfigurationBuilder()
+          //    .AddCommandLine(args)
+          //    .Build();
 
             var host = new WebHostBuilder()
                 .UseKestrel()
-                .UseConfiguration(config)
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
